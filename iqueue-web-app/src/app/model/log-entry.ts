76f02b10be8c5a48cb80f7e82591eac13404dc44
ISEL-A40569 <40569@alunos.isel.ipl.pt>
2020-09-26 16:12:51 +0100

    Some fixes.

--- iqueue-web-app/src/app/model/log-entry.ts
+++ iqueue-web-app/src/app/model/log-entry.ts
@@ -3,10 +3,6 @@ export class LogEntry {
     logCreationDateTime: Date
     requestMethod: string
     requestUri: string
-    requestHeaders: string
-    requestBody: string
     responseStatus: number
-    responseHeaders: string
-    responseBody: string
 }
 
